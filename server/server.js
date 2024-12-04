d2181f89358268061f2e5490dcae0b9295a0befe
IoanTimis <timisionut2000@gmail.com>
2024-12-04 17:05:46 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT = 8080;
+
+app.use(cors());
+
+
+app.get('/api/home', (req, res) => {
+  res.json({ message: 'Hello World', people: ['Ana', 'Maria', 'Ion'] });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
+
