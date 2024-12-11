9e9e7aa6806d044688be5685f5047c3627d4470a
IoanTimis <timisionut2000@gmail.com>
2024-12-11 12:40:55 +0200

    adding chain middleware, withUser middleware

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -1,41 +1,5 @@
-import { NextResponse } from 'next/server'
-console.log("A fost apelat middleware.js");
- 
-export function middleware(request) {
-  console.log("A fost apelat fucntia middleware");
-  const { pathname } = request.nextUrl; // Obține URL-ul curent
-  console.log("Middleware apelat pentru ruta:", pathname);
+import { stackMiddlewares } from "./middlewares/stackHandler";
+import { withUser } from "./middlewares/withUser";
 
-  const session = request.cookies.get("session"); // Obține cookie-ul `session`
-  console.log("Cookie session:", session);
-
-  if (!session) {
-    console.log("Utilizator nelogat - redirecționare la /auth/login");
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  } else {
-    const { role } = JSON.parse(session.value);
-    console.log("Utilizator logat cu rolul:", role);
-
-    if (pathname.startsWith("/admin") && role !== "admin") {
-      console.log("Redirecționare: Utilizatorul nu are rol admin");
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-
-    if (pathname.startsWith("/teacher") && role !== "teacher") {
-      console.log("Redirecționare: Utilizatorul nu are rol teacher");
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-
-    if (pathname.startsWith("/student") && role !== "student") {
-      console.log("Redirecționare: Utilizatorul nu are rol student");
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
-
-  console.log("Acces permis");
-  return NextResponse.next(); // Permite accesul
-}
- 
-export const config = {
-  matcher: '/teacher/:path*',
-}
+const middlewares = [withUser];
+export default stackMiddlewares(middlewares);
